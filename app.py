53e2273a88ba8130f1271021a3ef23512b3b87dc
Rhast-dev <emanuelcardonag22@gmail.com>
2026-05-17 21:37:48 -0500

    Configurando render

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, render_template, request, jsonify
 from pulp import *
 import json
@@ -230,4 +232,5 @@ def solve():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
